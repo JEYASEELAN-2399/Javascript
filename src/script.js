b65dc825fdab71807fb872d3530a445b500e3368
JEYASEELAN-2399 <115181970+JEYASEELAN-2399@users.noreply.github.com>
2023-11-21 19:45:43 +0530

    variables

--- src/script.js
+++ src/script.js
@@ -1,5 +1,34 @@
-import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-`;
+
+document.getElementById("app").innerHTML = "hello jeyaseelan";
+let car = 'suzuki'
+var carmodel = 2023;
+var carmodel = 2020;
+{
+    let car='swift';
+    console.log(car)
+    var carmodel = 2021
+}
+ car = 'bmw'
+
+console.log(car)
+console.log(typeof(carmodel))
+
+
+{
+  let  x = 20;
+  console.log(x)
+}
+
+
+{
+   let x = 10;
+   console.log(x)
+}
+
+
+// let and const is used to declare the variables and it is blocked scope 
+
+
+
+//we can redeclare variables in another diff block but in same block it is not allowed
